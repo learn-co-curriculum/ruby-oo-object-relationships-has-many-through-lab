8b7d4e11d526a9d9b6204f8b667c4d88fd3f0e06
David Polcari <dpolcari18@gmail.com>
2021-01-16 15:26:43 -0600

    all tests passing

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|songs| songs.genre == self}
+    end
+
+    def artists
+        Song.all.select {|songs| 
+            songs.genre == self}.map {|song_instances| 
+                song_instances.artist} 
+    end
+end
